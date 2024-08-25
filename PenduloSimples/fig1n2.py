c11b16822f8e281a3b35195e2a7403732ec55e0d
LXavier133 <lxddantas@gmail.com>
2024-08-25 18:10:52 -0300

    feat: experiment about gaussian distribution and continuous variables added, 100 measures with a stopwatch (cm) and a bench timer (cb) of 10 periods of a simple pendule

---
+++ PenduloSimples/fig1n2.py
@@ -0,0 +1,30 @@
+import numpy as np, matplotlib.pyplot as plt, math
+
+def gaussian(x,x0,variance):
+	return (1 / (np.sqrt(2 * np.pi * variance))) * np.exp(-((x - x0) ** 2) / (2 * variance))
+
+
+f=open("data_cm.txt","r") #change cm to cb to obtain the second figure
+lines=f.readlines()
+f.close()
+
+data=np.array([float(line.strip()) for line in lines[1:]])
+xm= np.mean(data)
+x2m=np.mean(data**2)
+variance = x2m - xm**2
+mini,maxi=np.min(data), np.max(data)
+
+d=max(xm-mini,maxi-xm)
+x= np.linspace(xm-d,xm+d,1000)
+y= gaussian(x,xm,variance)
+
+plt.xlabel("10T(s)")
+plt.ylabel("frequência")
+
+plt.hist(data,bins=math.ceil(1+math.log2(100)),edgecolor="black",color="white",density=True,label="frequência exp.")
+
+plt.plot(x,y,color="black",label="gaussiana")
+
+plt.legend(loc="upper left")
+plt.show()
+
